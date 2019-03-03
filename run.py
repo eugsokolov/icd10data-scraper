f77698166b7c7bd83f6aa27498639f288cd56522
Eugene Sokolov <eugsokolov@gmail.com>
2019-03-03 14:04:37 -0500

    Update run.py

--- run.py
+++ run.py
@@ -1,33 +1,9 @@
-'''
-Load ICD codes and find appopriate synonyms for a code
-source: www.icd10data.com
-
-Requirements:
-This web app scrapes icd10data and lazily stores codes and appropriate synonyms in a local mongo db
-
-You must run:
- $ pip install -r requirements.txt
-
-Usage:
-Run the app:
- $ python run.py
-
-Get a code's synonyms:
- $ curl http://localhost:8080/code/<code>
-
-Given a list of codes in codes.txt,
- $ for i in `cat codes.txt`; do curl http://localhost:8080/code/$i ; done
-
-(WIP) Load codes into a database - optional, but will prepopulate all codes:
- $ curl -X POST http://localhost:8080/load
-
-'''
 from main import app
 from icdscraper import loadCodes, get
 
 @app.route('/load', methods=['POST'])
 def setter():
-    final= loadCodes()
+    final = loadCodes()
     if not final:
         abort(404)
     return 'successfully loaded {} codes\n'.format(len(final))
